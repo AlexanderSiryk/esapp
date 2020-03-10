fb67e06db0c84640ddf89a3969870e2329b3b457
sntk14 <sntk14@ukr.net>
2020-03-10 23:01:16 +0200

    add back

---
+++ backend/database/migrations/2020_03_05_173117_create_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('name', 255);
+            $table->char('login', 255);
+            $table->char('password', 255);
+            $table->integer('tag');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
