e668edae17a8475ef434c61f39dd0332e8d6da5c
Genocity <gumption1543@gmail.com>
2020-03-29 20:08:20 +0300

    pre-branch

--- frontend/src/Components/Common/LoggedInCheckLayer/LoginPage/LoginPage.jsx
+++ frontend/src/Components/Common/LoggedInCheckLayer/LoginPage/LoginPage.jsx
@@ -4,7 +4,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
