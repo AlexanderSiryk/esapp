ea5cc9a3f74bcc8b97020ab52fa79723c602f3d0
unige <unigenocity@gmail.com>
2020-05-05 23:34:51 +0300

    removed unused components/folders/(other trash)

--- frontend/src/Components/Main/Content/FunctionalTable/FunctionalTable.jsx
+++ frontend/src/Components/Main/Content/FunctionalTable/FunctionalTable.jsx
@@ -1,8 +1,6 @@
 import DataTableContainer from "./DataTable/DataTableContainer";
 import React from "react";
 
-// TODO remove components: HeadRow, ModalWindowEditContainer, ModalWindowAddContainer
-
 let FunctionalTable = () => {
 	return <DataTableContainer/>
 }
