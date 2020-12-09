b57734a9d1a8bd7771be28079afbfa0b2ffe3f60
sntk14 <sntk14@ukr.net>
2020-12-09 20:36:14 +0200

    url change

--- frontend/src/API/DAL_API.js
+++ frontend/src/API/DAL_API.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const URL = "http://esapp/public";
+const URL = "http://127.0.0.1:8000/api";
 
 const server = {
     fetchPasswords(token) {
@@ -27,7 +27,7 @@ const server = {
             .catch(error => error);
     },
     editPassword(id, data) {
-        return axios.patch(`${URL}/accounts/${id}`, data)
+        return axios.put(`${URL}/accounts/${id}`, data)
             .then(res => res)
             .catch(error => error);
     },
