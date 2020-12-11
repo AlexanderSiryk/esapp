1048185bc42a8d75511843d6b87eef30d8b20411
sntk14 <sntk14@ukr.net>
2020-12-11 23:05:23 +0200

    access login

--- app/Repositories/AddRepository.php
+++ app/Repositories/AddRepository.php
@@ -17,13 +17,12 @@ class AddRepository
             ->where('user_id', $userId)
             ->get();
 
-        if(!$entrs) return [];
+        if (!$entrs) return [];
         return self::toJSON($entrs);
     }
 
 
-
-    public static function setEnter($userId,$location)
+    public static function setEnter($userId, $location)
     {
         return DB::table('entrances')
             ->insert(
@@ -36,10 +35,46 @@ class AddRepository
     }
 
 
-    private static function toJSON($array){
+    public static function getLastEnter($userId)
+    {
+        $res = DB::table('entrances','id')
+            ->select(['location','id'])
+            ->where('user_id', $userId)
+            ->orderBy('id', 'DESC')
+            ->limit(1)
+            ->get();
+
+        return self::toJSON($res,1)[0];
+    }
+
+
+    public static function getRandomEnter($id, $location, $count = 4)
+    {
+        $res = DB::table('entrances','id')
+            ->select(['id','location'])
+            ->where('location', '!=', $location)//['user_id', $id]
+            ->inRandomOrder()
+            ->limit($count)
+            ->get();
+
+        return self::toJSON($res,1);
+    }
+
+
+    private static function toJSON($array, $ext = 0)
+    {
         $arr = [];
-        foreach ($array as $item){
-            $arr[] = json_decode($item->location);
+        foreach ($array as $item) {
+            if($ext){
+                $obj = (object)json_decode($item->location);
+                $obj->id = $item->id;
+
+                $arr[] = $obj;
+            }
+            else{
+                $arr[] = json_decode($item->location);
+            }
+
         }
         return $arr;
     }
@@ -64,6 +99,48 @@ class AddRepository
                 ]);
 
     }
+
+
+
+    public static function getLogRequest($userId)
+    {
+        $entrs = DB::table('log_request')
+            ->select(['id','answer','log','access'])
+            ->where('user_id', $userId)
+            ->orderBy('id','DESC')
+            ->limit(1)
+            ->first();
+
+        if(!$entrs) return (object)['access'=>1];
+
+        return $entrs;
+    }
+
+    public static function changeLogRequest($id, $success)
+    {
+        DB::table('log_request')
+
+            ->where('id', $id)
+            ->update(['access' => $success]);
+
+        return $success;
+    }
+
+    public static function setLogRequest($userId, $answer, $log)
+    {
+        return DB::table('log_request')
+            ->insert(
+                [
+                    'user_id' => $userId,
+                    'date' => date("Y-m-d"),
+                    'access' => 0,
+                    'answer' => $answer,
+                    'log' => $log,
+                ]);
+
+    }
+
+
 
 
 }
